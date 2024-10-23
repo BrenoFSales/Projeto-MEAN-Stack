29be7c61fab3d51a9d63aef720b2ddc001201818
silva guimaraes <silva__guimaraes@hotmail.com>
2024-10-23 16:57:52 -0300

    mensagens não sendo exibidas corretamente

--- frontend/app-stack-mean/src/messages/messages-list.component.ts
+++ frontend/app-stack-mean/src/messages/messages-list.component.ts
@@ -17,20 +17,20 @@ import { Message } from './messages.model';
   template: `
     <div class="col-md-8 col-md-offset-2">
       @for (msg of messageS; track $index) {
-        <app-messages>
-        </app-messages> 
+        <app-messages [messageVarClasse]="msg">
+        </app-messages>
       } @empty {
-        messageS é uma lista vazia
+        menssages é uma lista vazia
       }
     </div>
   `,
-  // OLD TEMPLATE: 
-  /* 
+  // OLD TEMPLATE:
+  /*
     <div class="col-md-8 col-md-offset-2">
       @for (msg of messageS; track $index) {
         <app-messages [messageVarClasse]="msg"
                       (outputMessage)="msg.content = $event">
-        </app-messages> 
+        </app-messages>
       } @empty {
         messageS é uma lista vazia
       }
@@ -41,13 +41,13 @@ import { Message } from './messages.model';
 
 export class MessagesListComponent implements OnInit {
   messageS: Message[] = [];
-  
+
   constructor (private messageService: MessageService){ }
-  
+
   ngOnInit(): void {
     // messageS aponta para o array messageService que armazena os dados
     // this.messageS = this.messageService.getMessage(); // lista no frontend(próprio service)
-    
+
     // metodo para fazer o get no backent
     this.messageService.getMessage()
       .subscribe({
@@ -55,7 +55,7 @@ export class MessagesListComponent implements OnInit {
           console.log(dadosSucesso.myMsgSucesso);
           console.log({content: dadosSucesso.objSMessageSRecuperadoS[0].content});
           console.log({id: dadosSucesso.objSMessageSRecuperadoS[0].messageId});
-          
+
           this.messageS = dadosSucesso.objSMessageSRecuperadoS;
         },
         error: (dadosErro: any) => {
--- frontend/app-stack-mean/src/messages/messages.component.ts
+++ frontend/app-stack-mean/src/messages/messages.component.ts
@@ -14,17 +14,17 @@ import { MessageService } from './messages.services';
 export class MessagesComponent {
   // @Input() messageVarClasse : Message = new Message("","");
   messageVarClasse = input<Message>(new Message("",""));
-  
+
   editMode: boolean = false; // Estado que define se a mensagem está em modo edição ou não
   mensagemEditada: string = ''; // Texto da mensagem que será editado
-  
+
   ngOnInit() {
     this.mensagemEditada = this.messageVarClasse().content; // Copia a mensagem que será editada para o textarea
   }
-  
-  
+
+
   constructor(private messageServiceObj: MessageService) { }
-  
+
   // Função para editar a mensagem
   editarMensagem(){
     // this.outputMessage.emit("Texto retornado componente mensagens!");
@@ -39,13 +39,13 @@ export class MessagesComponent {
       this.editMode = true;
     }
   }
-  
+
   // Função para cancelar a edição da mensagem
   cancelarEdicao(){
     this.editMode = false; // sai do modo edição da mensagem
     this.mensagemEditada = this.messageVarClasse().content; // reseta a variável mensagemEditada
   }
-  
+
   excluirMensagem(){
     this.messageServiceObj.deleteMessage(this.messageVarClasse());
   }
